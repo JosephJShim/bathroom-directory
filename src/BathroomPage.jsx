520d2dcc6bb773bc9fc570919bfc3d186fb31cce
taeoldest <alextaeyounglee@gmail.com>
2023-06-04 23:25:16 -0500

    finished algorithm and formatting for mobile, for joseph to fix map

--- src/BathroomPage.jsx
+++ src/BathroomPage.jsx
@@ -25,22 +25,22 @@ function BathroomPage() {
 
     return (
         <div className="page-bg1">
-          <div className="page-bg2">
-            <h1 className="nu-bathroom-finder">NU Bathroom Finder</h1>
-            <div>
-              <h3>{bathroom["Building Nickname"]}, Floor {bathroom.Floor}, Room {bathroom.Room}, {bathroom.Gender}</h3>
-              <h2>
-                <a href={bathroom.GoogleMaps}>Google Maps Link</a>
-              </h2>
-              <p style={{ maxWidth: "75%", wordWrap: "break-word", margin: "auto" }}>Extra Notes: {bathroom.ExtraInfo}</p>
-              <p>Features: {features.join(', ')}</p>
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLSf7uEkCziYhchcrtnIp5jnlvLtacd7PO98NMCfKPH4ciTXkTw/viewform?usp=sf_link">
-                Bathroom Feedback Form →
-              </a>
+            <div className="page-bg2">
+                <h1 className="nu-bathroom-finder bathroom-page-text">NU Bathroom Finder</h1>
+                <div>
+                    <h3 className="bathroom-page-text">{bathroom["Building Nickname"]}, Floor {bathroom.Floor}, Room {bathroom.Room}, {bathroom.Gender}</h3>
+                    <h2 className="bathroom-page-text">
+                        <a href={bathroom.GoogleMaps} className="bathroom-page-text">Google Maps Link →</a>
+                    </h2>
+                    <p className="bathroom-page-text" style={{ maxWidth: "75%", wordWrap: "break-word", margin: "auto" }}>Extra Notes: {bathroom.ExtraInfo}</p>
+                    <p className="bathroom-page-text">Features: {features.join(', ')}</p>
+                    <a className="bathroom-page-text" href="https://docs.google.com/forms/d/e/1FAIpQLSf7uEkCziYhchcrtnIp5jnlvLtacd7PO98NMCfKPH4ciTXkTw/viewform?usp=sf_link">
+                        Bathroom Feedback Form →
+                    </a>
+                </div>
             </div>
-          </div>
         </div>
-    );
+    );    
 }
 
 export default BathroomPage;
